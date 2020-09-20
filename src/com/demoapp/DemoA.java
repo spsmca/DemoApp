e28cec4d3fcc1fcc888b83ef96909062149cfcb2
spsmca <71463984+spsmca@users.noreply.github.com>
2020-09-20 12:24:20 +0530

    Update DemoA.java

--- src/com/demoapp/DemoA.java
+++ src/com/demoapp/DemoA.java
@@ -3,7 +3,7 @@ package com.demoapp;
 public class DemoA {
 
 	public static void main(String[] args) {
-		System.out.println("This is : DemoA");
+		System.out.println("This is : DemoA  (Branch1)" );
 
 	}
 
