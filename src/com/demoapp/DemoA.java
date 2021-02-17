15a333720469854b9d0a0664de1ed8beec233dcb
spsmca <71463984+spsmca@users.noreply.github.com>
2021-02-17 21:44:44 +0530

    Update DemoA.java

--- src/com/demoapp/DemoA.java
+++ src/com/demoapp/DemoA.java
@@ -5,8 +5,9 @@ public class DemoA {
 	public static void main(String[] args) {
     
     // merged as below
-		System.out.println("This is : DemoA  (Branch1)" );
+          System.out.println("This is : DemoA  (Branch1)" );
 	  System.out.println("This is : Branch2");
+	  System.out.println("This is : Branch3");
 
 	}
 }
