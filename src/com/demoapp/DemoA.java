893f7764e10efe05ad61bc9ea63197d6260989f9
spsmca <sps_mca@rediffmail.com>
2021-02-18 21:45:57 +0530

    Added to Branch2

--- src/com/demoapp/DemoA.java
+++ src/com/demoapp/DemoA.java
@@ -7,7 +7,7 @@ public class DemoA {
     // merged as below
           System.out.println("This is : DemoA  (Branch1)" );
 	  System.out.println("This is : Branch2");
-	  System.out.println("This is : Branch3");
+	  System.out.println("This is : Branch4");
 
 	}
 }
